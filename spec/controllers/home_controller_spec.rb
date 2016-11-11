b56889d463c3a01fae14ebaf1ef12e3def04e3c8
Carine Vunabandi <carine.vunabandi@bt.com>
2016-11-11 16:40:37 +0000

    add the homepage view file and dependencies

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe HomeController do
   describe "index" do
     it "renders the index page" do
-      expect(get :index).to render_template("index")
+      expect(get :index).to render_template :"home/index"
     end
   end
 end
