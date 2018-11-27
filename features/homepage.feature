246b4a7138f4f79dfa6fd60ff663b2a75a08481b
Carine Vunabandi <--replace-all>
2018-11-27 16:53:19 +0000

    fix homepage feature tests

--- features/homepage.feature
+++ features/homepage.feature
@@ -3,8 +3,6 @@ Feature: Viewing the homepage
   Background:
     When I view the homepage
 
-
-    # @wip
   Scenario: Viewing the homepage tabs
     Then I see the 'current budget' tab
     Then I see 'past budgets' tab
--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -8,11 +8,11 @@ Given "I am on the homepage" do
 end
 
 Then "I see the 'current budget' tab" do
-  expect(@homepage).to have_link("Current Expenses")
+  expect(@homepage).to have_link("Current")
 end
 
 Then "I see 'past budgets' tab" do
-  expect(page.body).to have_link("Past Expenses")
+  expect(page.body).to have_link("Past")
 end
 
 Then "I see the author's email address" do
