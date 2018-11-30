5187cc96fa39a8365b6abcaa63ae591b051b68f9
Carine Vunabandi <--replace-all>
2018-11-30 21:03:05 +0000

    remove this line as no longer necessary in rails 5

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module RailsShekelTracker
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.generators do |g|
       g.factory_bot false
     end
